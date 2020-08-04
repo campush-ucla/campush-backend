8994967e1e7f0ef25c8a3036bbadbe3204949975
Qing Shi <aliceshi0804@gmail.com>
2020-08-03 21:55:16 -0700

    added search return info and search a certain club

--- routes/search.js
+++ routes/search.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 const router = express.Router();
 const { client } = require("../database/db");
+const { json } = require("express");
 
 //return all club info
 router.get("/allclubs", async function (req, res) {
@@ -29,7 +30,7 @@ router.get("/allnames", async function (req, res) {
   }
 });
 
-// return all clubs within a certain category
+// return all clubs with descriptions and emails within a certain category
 router.get("/clubs", async function (req, res) {
   let db = client.db("campush");
   let collection = db.collection("clubs");
@@ -37,7 +38,25 @@ router.get("/clubs", async function (req, res) {
   let clubs = await collection
     .find(
       { "Category": {$regex: new RegExp(`^${category}$`, 'i')} },
-      { projection: { "Organization Name": 1, _id: 0 } }
+      { projection: { "Organization Name": 1, "Organization Description": 1, "Email": 1, _id: 0 } }
+    )
+    .toArray();
+  if (clubs !== null) {
+    res.json(clubs);
+  } else {
+    res.json("Not found");
+  }
+});
+
+// return club name, description and email for a given club
+router.get("/club", async function (req, res) {
+  let db = client.db("campush");
+  let collection = db.collection("clubs");
+  const name = req.query.name;
+  let clubs = await collection
+    .find(
+      { "Organization Name": {$regex: new RegExp(`^${name}$`, 'i')} },
+      { projection: { "Organization Name": 1, "Category": 1, "Organization Description": 1, "Email": 1, _id: 0 } }
     )
     .toArray();
   if (clubs !== null) {
